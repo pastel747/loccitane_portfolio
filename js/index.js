5ed9d287c12d915e3d2c26eb5678efd1d72cd917
yeon <68599757+pastel747@users.noreply.github.com>
2020-12-03 17:37:35 +0900

    메인 css 진행중

--- js/index.js
+++ js/index.js
@@ -1,12 +1,28 @@
 $(document).ready(function(){
     swiperSetting();
+    swiperSetting2();
 });
 
 function swiperSetting(){
-    var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 1,
-    pagination: {
-        el: '.swiper-pagination',
-      }
-    });
+    // var swiper = new Swiper('.swiper-container', {
+    // slidesPerView: 1,
+    // loop: true,
+    // pagination: {
+    //     el: '.swiper-pagination',
+    //     clickable: true,
+    //   },
+    // }); 
+}
+function swiperSetting2(){
+    var swiper2 = new Swiper('.name .swiper-container', {
+        slidesPerView: 3,
+        navigation: {
+            nextEl: '.name .swiper-button-next',
+            prevEl: '.name .swiper-button-prev',
+          },
+        pagination: {
+            el: '.name .swiper-pagination',
+            clickable: true,
+          },
+        });
 }
---
+++ test.md
@@ -0,0 +1,23 @@
+그리드, 플로트, 플렉스
+
+부모에 display flex
+자녀들은 flexitem이 된다.
+보통 인라인 플렉스 보다 그냥 플렉스를 준다.
+플렉스는 display block과 같다.
+flex direction은 float left right 로우 칼럼 인버스
+flex wrap은 wrap nowrap만..
+nowrap이 기본이고 wrap이 플로트 처럼 떨어진다.
+flexflow는 축약법
+
+justcontent flex엔드  
+스페이스 비트윈은 알아서 자녀가 양옆으로 갖는 패딩?이 똑같은거
+스페이스 어라운드는 비트윈이랑 같은데 첫번째 마지막에도 마진이 있다 
+스페이스 evenly는 어라운드랑 다르게 간격이 전부 동일하다
+align item 스트레치 센터 엔드 스타트 만 주로 쓴다
+align item 은 개별적인거 align content는 좀 더 전체적인거
+align self는 개별적인거 잡아서 하나만 바꾸고 싶을때
+
+flex :숫자는 축약법 된거임 li에 정해놓고 li 개별 잡아서 값을 주면 
+flex basis:100px 베이시스는 크기이다 이게 플렉스보다 더 우선순위에 있다. 
+
+오토 리네임 태그
